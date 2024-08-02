4b459bf1ac7fbf3e3a27cabc6e2e20e40e4e1502
BikrantJung <bikrant11bit23@kcc.edu.np>
2024-08-02 12:34:50 +0545

    feat: choose directory

---
+++ src/ui/file_picker.c
@@ -0,0 +1,28 @@
+#include <gtk/gtk.h>
+
+static void file_dialog_response(GtkNativeDialog *dialog, int response,
+                                 gpointer user_data) {
+  if (response == GTK_RESPONSE_ACCEPT) {
+    g_autoptr(GFile) file = gtk_file_chooser_get_file(GTK_FILE_CHOOSER(dialog));
+    g_print("File selected: %s\n", g_file_get_path(file));
+  }
+  gtk_native_dialog_destroy(dialog);
+}
+
+static void open_file_dialog(GtkWidget *widget, gpointer window) {
+  GtkFileChooserNative *dialog = gtk_file_chooser_native_new(
+      "Select a File", GTK_WINDOW(window),
+      GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER, "_Open", "_Cancel");
+
+  g_signal_connect(dialog, "response", G_CALLBACK(file_dialog_response), NULL);
+  gtk_native_dialog_show(GTK_NATIVE_DIALOG(dialog));
+}
+
+GtkWidget *create_file_chooser() {
+
+  GtkWidget *button = gtk_button_new_with_label("Select Folder");
+
+  g_signal_connect(button, "clicked", G_CALLBACK(open_file_dialog), NULL);
+
+  return button;
+}
--- src/ui/setup_ui.c
+++ src/ui/setup_ui.c
@@ -3,5 +3,16 @@ void setup_ui(GtkApplication *app, gpointer user_data) {
   GtkWidget *window = gtk_application_window_new(app);
   gtk_window_set_title(GTK_WINDOW(window), "File Auto Sort");
   gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
+
+  // Create a box for file chooser:
+  GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+  GtkWidget *file_chooser = create_file_chooser();
+
+  // Append file chooser inside the box
+  gtk_box_append(GTK_BOX(box), file_chooser);
+
+  // Append box inside window
+  gtk_window_set_child(GTK_WINDOW(window), box);
+
   gtk_widget_show(window);
 }
